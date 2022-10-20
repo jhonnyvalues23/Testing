1f23479c080bd68b927385b142d775d5e92304ee
It's_Ali_Kazmi_512 <35180566+jhonnyvalues23@users.noreply.github.com>
2022-10-20 13:40:57 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -142,4 +142,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_on_heroku
 django_heroku.settings(locals())
